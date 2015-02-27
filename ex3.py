76f39859070334e414ecb6d887d68dc11a9c4388
Christopher Walsh <walshification@gmail.com>
2015-02-26 21:55:23 -0600

    exercise 3

---
+++ ex3.py
@@ -0,0 +1,43 @@
+# Prints the line in the terminal.
+print "I will now count my chickens:"
+
+# Prints the line and then adds 25 and 30 and divides the sum by 6.
+print "Hens", 25 + 30 / 6
+# Prints the word and subtracts 25 from 100, multiples the sum by 3 and
+# returns the remainder after that product is divided by 4.
+print "Roosters", 100 - 25 * 3 % 4
+
+# Prints the line.
+print "Now I will count my eggs:"
+
+# Calculates modulo: 4 % 2 (3 + 2 + 1 - 5 + 0 - 1 / 4 + 6),
+# performs division: 1 / 4 and rounds down to 0 (3 + 2 + 1 - 5 + 0 - 0 + 6),
+# performs addition: (7).
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# More accurate:
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Prints the line.
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Determines if this math is true (it is not).
+print 3 + 2 < 5 - 7
+
+# Prints the line; adds 3 to 2 for 5.
+print "What is 3 + 2?", 3 + 2
+# Prints the line; subtracts 7 from 5 for -2.
+print "What is 5 - 7?", 5 - 7
+
+# Prints a sudden realization.
+print "Oh, that's why it's False."
+
+# Prints the line.
+print "How about some more."
+
+# Prints the question and determines if 5 is greater than -2 (it is).
+print "Is it greater?", 5 > -2
+# Prints the question and determines if 5 is greater or equal to -2 (yes).
+print "Is it greater or equal?", 5 >= -2
+# Prints the question and determines if 5 is less than or equal to -2 (nope).
+print "Is it less or equal?", 5 <= -2
