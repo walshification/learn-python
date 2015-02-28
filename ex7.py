26169d23236309965956fdb1a759f70c2977ee87
Christopher Walsh <walshification@gmail.com>
2015-02-28 16:47:22 -0600

    exercise 7: more printing

---
+++ ex7.py
@@ -0,0 +1,28 @@
+# Prints the line.
+print "Mary had a little lamb."
+# Prints the line and interpolates the string snow with the formatter.
+print "Its fleece was white as %s." % 'snow'
+# Prints the line.
+print "And everywhere that Mary went."
+# Prints the string 10 times on the same line.
+print "." * 10
+
+# Initializes each of these variables with the corresponding values.
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# Concatenates the variable values together.
+# The comma keeps them on the same line. Removing it prints 1-6 and 7-12 on 
+# separate lines.
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
